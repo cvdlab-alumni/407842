4011e28922fbaa88397a5f755b06a3f0ec4ab0fe
Francesco Maglia <kikz.maglia@gmail.com>
2013-03-24 20:13:24 +0100

    Esercizi del 21 marzo

--- Learning-JavaScript/2013-03-21/Exercise02.js
+++ Learning-JavaScript/2013-03-21/Exercise02.js
@@ -1,13 +1,13 @@
-function Point2d (x,y){
+function Point2d(x,y){
 	this.x = x;
 	this.y = y;
 }
 
-function Edge(vertx1,vertx2){
-	this.vertx1 = vertx1;
-	this.vertx2 = vertx2;
+function Edge(p1,p2){
+	this.p1 = p1;
+	this.p2 = p2;
 }
 
-Edge.prototype.length = function(point1, Point2) {
-	Math.sqrt(Math.pow( (point2.x - Point1.x), 2) + Math.pow( (point2.y - Point1.y), 2)) 
+Edge.prototype.length = function() {
+	return Math.sqrt(Math.pow((this.p2.x - this.p1.x),2) + Math.pow((this.p2.y - this.p1.y), 2)); 
 }
