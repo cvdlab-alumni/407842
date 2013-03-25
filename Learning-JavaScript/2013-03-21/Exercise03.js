1b8365b4f0cf6cadb1a37aa16903f554aa6ec016
Francesco Maglia <kikz.maglia@gmail.com>
2013-03-25 11:50:52 +0100

    Modifica esercizio 3 esercizi

--- Learning-JavaScript/2013-03-21/Exercise03.js
+++ Learning-JavaScript/2013-03-21/Exercise03.js
@@ -1,9 +1,9 @@
-function Point2d(x,y){
+function Point2d (x,y) { 
 	this.x = x;
 	this.y = y;
 }
 
-function Edge(p1,p2){
+function Edge (p1,p2) {
 	this.p1 = p1;
 	this.p2 = p2;
 }
@@ -12,7 +12,7 @@ Edge.prototype.length = function() {
 	return Math.sqrt(Math.pow((this.p2.x - this.p1.x),2) + Math.pow((this.p2.y - this.p1.y), 2)); 
 }
 
-function Triangle(edge1,edge2,edge3){
+function Triangle (edge1,edge2,edge3) {
 	this.edge1 = edge1;
 	this.edge2 = edge2;
 	this.edge3 = edge3;
@@ -24,7 +24,7 @@ Triangle.prototype.perimeter = function() {
 }
 
 Triangle.prototype.euroneArea = function() {
-	var semiperimeter = (this.edge1.length() + this.edge2.length() + this.edge3.length()) / 2;
+	var semiperimeter = this.perimeter() / 2;
 	var area = Math.sqrt((semiperimeter * (semiperimeter - this.edge1.length()) * (semiperimeter - this.edge2.length()) * (semiperimeter - this.edge3.length()) ));
 	return area;
 }
